112f8e79e23a4d21db90851de4d6ae6f08a25dbd
bitlowsky <bitlowsky@gmail.com>
2020-03-05 21:39:31 +0300

    Create bot package

---
+++ bot/handlers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from telegram import Update
+from telegram.ext import CallbackContext
+from bot.log import logger
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+
+
+def start(update: Update, context: CallbackContext):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
+
+
+def chat_help(update: Update, context: CallbackContext):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Введи команду /start для начала. ')
+
+
+def echo(update: Update, context: CallbackContext):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update: Update, context: CallbackContext):
+    """Log Errors caused by Updates."""
+    logger.warning(f'Update {update} caused error {context.error}')
--- main.py
+++ main.py
@@ -1,47 +1,13 @@
-#!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
-
-import logging
-
 from setup import PROXY, TOKEN
-from telegram import Bot, Update
-from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-
-
-def start(update: Update, context: CallbackContext):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
-
-
-def chat_help(update: Update, context: CallbackContext):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Введи команду /start для начала. ')
-
-
-def echo(update: Update, context: CallbackContext):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def error(update: Update, context: CallbackContext):
-    """Log Errors caused by Updates."""
-    logger.warning(f'Update {update} caused error {context.error}')
+from telegram import Bot
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from bot.handlers import start, chat_help, echo, error
+from bot.log import logger
 
 
 def main():
-    bot = Bot(
-        token=TOKEN,
-        base_url=PROXY,  # delete it if connection via VPN
-    )
+    bot = Bot(token=TOKEN, base_url=PROXY)
     updater = Updater(bot=bot, use_context=True)
 
     # on different commands - answer in Telegram
