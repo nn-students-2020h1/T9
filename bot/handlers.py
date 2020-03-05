aa9714005e3380c190a31a82dbb6bda78af08832
bitlowsky <bitlowsky@gmail.com>
2020-03-05 22:07:49 +0300

    Add bot logger

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,27 +1,31 @@
 # -*- coding: utf-8 -*-
 from telegram import Update
 from telegram.ext import CallbackContext
-from bot.log import logger
+from bot.log import logger, log
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
 
+@log
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
     update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
 
 
+@log
 def chat_help(update: Update, context: CallbackContext):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Введи команду /start для начала. ')
 
 
+@log
 def echo(update: Update, context: CallbackContext):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 
+@log
 def error(update: Update, context: CallbackContext):
     """Log Errors caused by Updates."""
     logger.warning(f'Update {update} caused error {context.error}')
--- bot/log.py
+++ bot/log.py
@@ -6,3 +6,28 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+LOG_ACTIONS = []
+
+
+def log(function):
+    def inner(*args, **kwargs):
+        update = args[0]
+        i = len(LOG_ACTIONS)
+        LOG_ACTIONS.append({
+            "date": update["message"]["date"],
+            "call": function.__name__,
+            "user": update.effective_user,
+            "text": update["message"]["text"]
+        })
+        print(
+            f"{LOG_ACTIONS[i]['date']} |",
+            f"call: {LOG_ACTIONS[i]['call']} |",
+            f"id: {LOG_ACTIONS[i]['user']['id']} |",
+            f"user: {LOG_ACTIONS[i]['user']['username']}",
+            f"({LOG_ACTIONS[i]['user']['first_name']}",
+            f"{LOG_ACTIONS[i]['user']['last_name']}) |",
+            f"text: {LOG_ACTIONS[i]['text']}"
+        )
+        return function(*args, **kwargs)
+    return inner
