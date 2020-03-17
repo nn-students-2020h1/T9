b20c6819b919e8bac332d891614ef417fadbd63e
jswark <swark.a.j@gmail.com>
2020-03-17 23:19:23 +0300

    Comfortable buttons for fact/image/quotes/history

--- Buttons2.py
+++ Buttons2.py
@@ -3,13 +3,17 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup
 BUTTON_HISTORY = "HISTORY"
 BUTTON_QUOTES = "QUOTES"
 BUTTON_FACT = "FACT"
+BUTTON_IMAGE = "IMAGE"
 
 def reply_keyboard():
     keyboard = [
         [
             KeyboardButton(BUTTON_HISTORY),
             KeyboardButton(BUTTON_QUOTES),
+        ],
+        [
             KeyboardButton(BUTTON_FACT),
+            KeyboardButton(BUTTON_IMAGE),
         ],
     ]
     return ReplyKeyboardMarkup(
--- bot/handlers.py
+++ bot/handlers.py
@@ -2,19 +2,18 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+from Buttons2 import BUTTON_HISTORY, BUTTON_QUOTES, BUTTON_FACT, BUTTON_IMAGE, reply_keyboard
 from bot.log import dataBase, log, logger
 from modules import content
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
-
 @log
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
     update.message.reply_text(
         f'Привет, {update.effective_user.first_name}!\nСписок команд: /help')
-
 
 @log
 def chat_help(update: Update, context: CallbackContext):
@@ -27,8 +26,7 @@ def chat_help(update: Update, context: CallbackContext):
     /cat - картинка котика
     /fact - популярный факт о котах'''
 
-    update.message.reply_text(msg)
-
+    update.message.reply_text(msg, reply_markup=reply_keyboard())
 
 @log
 def history(update: Update, context: CallbackContext):
@@ -42,14 +40,19 @@ def history(update: Update, context: CallbackContext):
 
     update.message.reply_text(msg)
 
-
 @log
 def echo(update: Update, context: CallbackContext):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+   if update.message.text == BUTTON_HISTORY:
+        return history(update=update, context=context)
+   elif update.message.text == BUTTON_QUOTES:
+       return sendQuote(update=update, context=context)
+   elif update.message.text == BUTTON_FACT:
+       return sendCatFact(update=update, context=context)
+   elif update.message.text == BUTTON_IMAGE:
+       return sendCatImage(update=update, context=context)
+   """Echo the user message."""
+   update.message.reply_text(update.message.text)
 
-
-@log
 def sendQuote(update: Update, context: CallbackContext):
     """Send a message when the command /quote is issued."""
     text, author, book, person = content.getQuote()
@@ -64,18 +67,13 @@ def sendQuote(update: Update, context: CallbackContext):
 
     update.message.reply_text(msg)
 
-
-@log
 def sendCatImage(update: Update, context: CallbackContext):
     """Send a photo when the command /cat is issued."""
     update.message.reply_photo(content.getCatImage())
 
-
-@log
 def sendCatFact(update: Update, context: CallbackContext):
     """Send a text when the command /fact is issued."""
     update.message.reply_text(content.getCatFact())
-
 
 @log
 def error(update: Update, context: CallbackContext):
