a343bb6a6b8d5b748bd659a8a0364538ff2e1dc7
AlexandraPar <sasha-bell98@yandex.ru>
2020-03-25 02:07:23 +0300

    Update Buttons2.py

--- bot/Buttons2.py
+++ bot/Buttons2.py
@@ -1,13 +1,23 @@
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 
-BUTTON_HISTORY = "/history"
-BUTTON_QUOTES = "/quote"
-BUTTON_FACT = "/fact"
-BUTTON_IMAGE = "/cat"
+BUTTON_HISTORY = "request history"
+BUTTON_QUOTES = "quote"
+BUTTON_FACT = "fact about cats"
+BUTTON_IMAGE = "cat photo"
+BUTTON_HELP = "help"
+BUTTON_CORONA_STATUS = " highest infection rate"
+BUTTON_COUNTRY_STATUS = "most infected country"
 
 
 def reply_keyboard():
     keyboard = [
+        [
+            KeyboardButton(BUTTON_HELP)
+        ],
+        [
+            KeyboardButton(BUTTON_CORONA_STATUS),
+            KeyboardButton(BUTTON_COUNTRY_STATUS)
+        ],
         [
             KeyboardButton(BUTTON_HISTORY),
             KeyboardButton(BUTTON_QUOTES),
