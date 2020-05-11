cc4cfbfa5e1ea6e14454abc518142268b6335265
bitlowsky <bitlowsky@gmail.com>
2020-05-11 15:29:58 +0300

    add try/catch in format_date

--- content/utils.py
+++ content/utils.py
@@ -9,8 +9,12 @@ def get_history(user_id, count):
 
 def format_date(date):
     res = re.findall(r"(0?[1-9]|[12][0-9]|3[01])[\- ./](0?[1-9]|1[012])[\- ./](20[0-9]{2}|[0-9]{4}|[0-9]{2})", date)
-    day = res[0][0].replace('0', '')
-    month = res[0][1].replace('0', '')
-    year = res[0][2][-2:]
 
-    return f'{month}/{day}/{year}'
+    try:
+        day = res[0][0].replace('0', '')
+        month = res[0][1].replace('0', '')
+        year = res[0][2][-2:]
+        return f'{month}/{day}/{year}'
+
+    except Exception:
+        return ''
