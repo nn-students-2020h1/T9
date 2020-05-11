77cce7d737ac494566fda7fd0d056d0f6d733292
bitlowsky <bitlowsky@gmail.com>
2020-05-11 17:27:15 +0300

    fix format_date

--- content/utils.py
+++ content/utils.py
@@ -11,9 +11,11 @@ def format_date(date):
     res = re.findall(r"(0?[1-9]|[12][0-9]|3[01])[\- ./](0?[1-9]|1[012])[\- ./](20[0-9]{2}|[0-9]{4}|[0-9]{2})", date)
 
     try:
-        day = res[0][0].replace('0', '')
-        month = res[0][1].replace('0', '')
-        year = res[0][2][-2:]
+        day, month, year = res[0]
+        day = day[1] if day[0] == '0' else day
+        month = month[1] if month[0] == '0' else month
+        year = year[-2:]
+
         return f'{month}/{day}/{year}'
 
     except Exception:
