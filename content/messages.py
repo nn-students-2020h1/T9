13bbf0897ca0877903f963d29fa74fc3029fb6ca
bitlowsky <bitlowsky@gmail.com>
2020-05-16 19:23:41 +0300

    wiki info reduced to 3 sentences

--- content/messages.py
+++ content/messages.py
@@ -95,7 +95,7 @@ def image_recognition(tags: list) -> str:
 def wiki_info(query):
     try:
         summary, url = get_wiki_summary_with_db_check(query)
-        return summary + f'\n\n{url}'
+        return '. '.join(summary.split('. ')[:3]) + f'.\n\n{url}'
 
     except Exception:
         return 'Information not found. Try again.'
